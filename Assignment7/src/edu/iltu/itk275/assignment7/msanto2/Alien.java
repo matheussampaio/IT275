4f313c97a77767724aaac8a23a560f41d7f33f6b
Matheus Sampaio <matheus1401@gmail.com>
2015-10-26 13:12:15 -0500

    add assignment7

---
+++ Assignment7/src/edu/iltu/itk275/assignment7/msanto2/Alien.java
@@ -0,0 +1,79 @@
+/*
+ *  Filename:  Alien.java
+ *
+ *  Programmer: Matheus Sampaio
+ *  ULID: msanto2
+ *
+ *  Date: Oct 19, 2015
+ *
+ *  Class: IT275
+ *
+ */
+package edu.iltu.itk275.assignment7.msanto2;
+
+/**
+ * The Class Alien.
+ */
+public abstract class Alien {
+
+    /** The health. */
+    private int mHealth;
+
+    /** The name. */
+    private String mName;
+
+    /**
+     * Instantiates a new alien.
+     *
+     * @param name the name of the alien
+     * @param health the health of the alien
+     */
+    public Alien(String name, int health) {
+        setHealth(health);
+        setName(name);
+    }
+
+    /**
+     * Gets the health.
+     *
+     * @return the health
+     */
+    public int getHealth() {
+        return mHealth;
+    }
+
+    /**
+     * Sets the health.
+     *
+     * @param health the new health
+     */
+    public void setHealth(int health) {
+        mHealth = health;
+    }
+
+    /**
+     * Gets the name of the alien.
+     *
+     * @return the name of the alien
+     */
+    public String getName() {
+        return mName;
+    }
+
+    /**
+     * Sets the name of the alien.
+     *
+     * @param name the new name of the alien
+     */
+    public void setName(String name) {
+        mName = name;
+    }
+
+    /**
+     * Gets the damage.
+     *
+     * @return the damage
+     */
+    public abstract int getDamage();
+
+}
---
+++ Assignment7/src/edu/iltu/itk275/assignment7/msanto2/AlienPack.java
@@ -0,0 +1,64 @@
+/*
+ *  Filename:  AlienPack.java
+ *
+ *  Programmer: Matheus Sampaio
+ *  ULID: msanto2
+ *
+ *  Date: Oct 19, 2015
+ *
+ *  Class: IT275
+ *
+ */
+package edu.iltu.itk275.assignment7.msanto2;
+
+/**
+ * The Class AlienPack.
+ */
+public class AlienPack {
+
+    /** The aliens. */
+    private Alien[] mAliens;
+
+    /**
+     * Instantiates a new alien pack.
+     *
+     * @param size size of the pack
+     */
+    public AlienPack(int size) {
+        mAliens = new Alien[size];
+    }
+
+    /**
+     * Adds the alien.
+     *
+     * @param alien the alien
+     * @param index the index
+     */
+    public void addAlien(Alien alien, int index) {
+        mAliens[index] = alien;
+    }
+
+    /**
+     * Gets the aliens.
+     *
+     * @return the aliens
+     */
+    public Alien[] getAliens() {
+        return mAliens;
+    }
+
+    /**
+     * Calculate damage.
+     *
+     * @return damage amount of the pack
+     */
+    public int calculateDamage() {
+        int damage = 0;
+
+        for (Alien alien : mAliens) {
+            damage += alien.getDamage();
+        }
+
+        return damage;
+    }
+}
